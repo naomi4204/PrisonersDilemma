c35404c7c9e111454df47405b7691605f3ee742c
naomi4204 <44682263+naomi4204@users.noreply.github.com>
2018-11-06 13:03:30 -0700

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -380,3 +380,59 @@ def post_to_file(string, filename='tournament.txt', directory=''):
 if __name__ == '__main__':
     scores, moves, reports = main_play(modules[0:4])   
     section0, section1, section2, section3 = reports
+    -------------------------------------------------------------------------------
+Section 0 - Line up
+-------------------------------------------------------------------------------
+Player 0 (P0): E0, Collude
+        Always collude.
+Player 1 (P1): E1, Betray
+        Always betray.
+Player 2 (P2): E2, Alternate
+        Collude, then alternate.
+Player 3 (P3): E3, Collude but retaliate
+        Collude first round. Collude, except in a round after getting 
+        a severe punishment.
+-------------------------------------------------------------------------------
+Section 1 - Player vs. Player
+-------------------------------------------------------------------------------
+Each column shows pts/round earned against each other player:
+             P0     P1     P2     P3
+vs. P0 :      0    100     50      0
+vs. P1 :   -500      0   -376   -375
+vs. P2 :   -250    -75      0   -199
+vs. P3 :      0    -75   -199      0
+TOTAL  :   -750    -50   -525   -574
+
+-------------------------------------------------------------------------------
+Section 2 - Leaderboard
+-------------------------------------------------------------------------------
+Average points per round:
+Team name (P#):  Score      with strategy name
+E1        (P1):        -13 points with Betray                                  
+E2        (P2):       -132 points with Alternate                               
+E3        (P3):       -144 points with Collude but retaliate                   
+E0        (P0):       -188 points with Collude
+reports[3][0]    
+     scores, moves, reports = main_play([example1, example2, team0])
+      scores, moves, reports = main_play([example1]*3 + [example2]*2)
+# Example 0. Always collude
+def move(my_history, their_history, my_score, their_score):
+    return 'c'
+# Example 1. Always collude
+def move(my_history, their_history, my_score, their_score):
+    return 'c'
+# Example 2. Collude, then alternate
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history)%2 == 0:
+        return 'c'
+    else:
+        return
+# Example 3. Collude but retaliate
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if severely punished last time,
+    else:
+        return 'c' # otherwise collude
+           
